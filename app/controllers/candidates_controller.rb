9ad23b3badad2be984a28f214a6aead9078e0ffe
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-15 01:08:49 +0800

    005

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,11 +4,26 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
+
   end
 
   def create
+    # debugger
+    @candidate = Candidate.new(clean_params)
+
+    if @candidate.save
+      redirect_to '/candidates'
+    else
+      #NG
+    end
+
   end
 
+  # private
+  # def clean_params
+  # params[:candidate].require(:candidate).permit(:name , :party , :age , :politics)
+  # end
 
 
 
